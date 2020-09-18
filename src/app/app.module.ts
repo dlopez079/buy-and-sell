00d7e6f3635018490919616b0b4d64a817057e85
Dlopez079 <dlopez@lopezdavid.dev>
2020-09-18 02:36:20 -0400

    Added some components along with their routes to the project.  I also added some fake data so we can use for the project.  Lastly, I added global styles.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListingsPageComponent } from './listings-page/listings-page.component';
 import { RouterModule } from '@angular/router';
+import { ListingDetailPageComponent } from './listing-detail-page/listing-detail-page.component';
+import { EditListingPageComponent } from './edit-listing-page/edit-listing-page.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
+import { MyListingsPageComponent } from './my-listings-page/my-listings-page.component';
+import { NewListingsPageComponent } from './new-listings-page/new-listings-page.component';
 
 @NgModule({
-  declarations: [AppComponent, ListingsPageComponent],
+  declarations: [AppComponent, ListingsPageComponent, ListingDetailPageComponent, EditListingPageComponent, ContactPageComponent, MyListingsPageComponent, NewListingsPageComponent],
   imports: [BrowserModule, AppRoutingModule, RouterModule],
   providers: [],
   bootstrap: [AppComponent],
