bc65ae83593d4305e2a6b4ccca24d00599fbe873
Dlopez079 <dlopez@lopezdavid.dev>
2020-09-21 13:54:12 -0400

    Imported 'ActivatedRoute' from the angular package so I can use the tools to extract ID from the URL.  I imported the fakedata so we can use the data for the project.  I added a const called id to save the value of ID from the URL.

--- src/app/listing-detail-page/listing-detail-page.component.ts
+++ src/app/listing-detail-page/listing-detail-page.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router'; //Used to capture id from url.
+import { fakeListings } from '../fake-data'; //Used to import the fake data that we are using for this project.
 @Component({
   selector: 'app-listing-detail-page',
   templateUrl: './listing-detail-page.component.html',
-  styleUrls: ['./listing-detail-page.component.css']
+  styleUrls: ['./listing-detail-page.component.css'],
 })
 export class ListingDetailPageComponent implements OnInit {
-
-  constructor() { }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id'); //This line of code is going to capture the id from the url and store it in a const called id.
   }
-
 }
