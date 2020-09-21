f99d86328a6fd176b606146eae0439675c8f7cad
Dlopez079 <dlopez@lopezdavid.dev>
2020-09-21 15:27:13 -0400

    Completed my listings component ts

--- src/app/my-listings-page/my-listings-page.component.ts
+++ src/app/my-listings-page/my-listings-page.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { fakeMyListings } from '../fake-data'; //Used to import the fake mydata that we are using for this project.
+import { Listing } from '../types'; // Used to import listing from types.ts.
 
 @Component({
   selector: 'app-my-listings-page',
   templateUrl: './my-listings-page.component.html',
-  styleUrls: ['./my-listings-page.component.css']
+  styleUrls: ['./my-listings-page.component.css'],
 })
 export class MyListingsPageComponent implements OnInit {
+  listings: Listing[] = []; // Create a listing member variable.
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.listings = fakeMyListings;
   }
-
+  onDeleteClicked(listingId: string): void {
+    alert(`Deleting your listing with id ${listingId}`);
+  }
 }
